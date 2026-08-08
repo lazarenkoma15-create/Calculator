04ea16a2ac96cbb15b409de42d84a50c115ffa67
Михаил <laymonlm156gmail.com>
2026-08-08 19:14:16 +0300

    Сделана полностью верстка приложения

--- calculator/Viewes/MainWindow.xaml.cs
+++ calculator/Viewes/MainWindow.xaml.cs
@@ -22,5 +22,41 @@ namespace calculator
         {
             
         }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left)
+                return;
+
+            if (e.ClickCount == 2)
+            {
+                ToggleWindowState();
+                return;
+            }
+
+            DragMove();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleWindowState();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ToggleWindowState()
+        {
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
     }
 }
